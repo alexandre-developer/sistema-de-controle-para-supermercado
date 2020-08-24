2c322807d9aa15bf988bdd2e201c65e043198c25
alexandre de oliveira <69971563+alexandre-developer@users.noreply.github.com>
2020-08-24 12:05:49 -0300

    Classe conexao
    Esta classe de conexão é responsável pela conexão com o banco de dados sql-server

---
+++ conexao.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaDados
+{
+    class Conexao
+    {
+        public static string Cn = "Data Source = DESKTOP-UE2G19Q; Initial Catalog = dbcomercio; Integrated Security=true";
+    }
+}
